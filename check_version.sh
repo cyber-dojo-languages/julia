4e07f6db53e198f75889e6154563c52f002dc17e
Jon Jagger <jon@jaggersoft.com>
2020-09-25 13:37:53 +0100

    Now at 1.5.2

--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$(cd "$(dirname "${0}")" && pwd)"
-readonly EXPECTED=1.5.1
+readonly EXPECTED=1.5.2
 readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'julia --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
